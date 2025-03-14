7dc311f77d2dc59b6561528eb9e0edbe82016d88
Nat MacNell <142926700+nathanielmacnell@users.noreply.github.com>
2025-03-15 08:08:39 +1300

    :bug: fix clang error from undeclared variable

--- src/poLCA.c
+++ src/poLCA.c
@@ -32,6 +32,7 @@ void ylik(double *probs, int *y, int *obs, int *items,
 	const int citems = *items;
 	const int cclasses = *classes;
 	const int cobs = *obs;
+	const double DOUBLE_XMAX;
 	const double *firstprobs = probs;
 
 	for (i=0;i<cobs;i++) {
